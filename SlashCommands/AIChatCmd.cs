37f8a9c0ffd3fa27926ef085d1f895f858239144
Kris <krisquack@outlook.com>
2023-01-28 01:02:05 +0000

    Sanitize AI inputs

--- SlashCommands/AIChatCmd.cs
+++ SlashCommands/AIChatCmd.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
@@ -25,7 +26,7 @@ public class AIChatCmd : InteractionModuleBase
         var typingObj = Context.Channel.EnterTypingState();
         var settings = Setting.GetSettings();
         //Get chat to send
-        var messages = await Context.Channel.GetMessagesAsync(procCount).FlattenAsync();
+        var messages = await Context.Channel.GetMessagesAsync(procCount, CacheMode.CacheOnly).FlattenAsync();
         var queryString = pretext + "\n\n";
         //Ignore embeds and media
         messages = messages.Where(m =>
@@ -36,7 +37,7 @@ public class AIChatCmd : InteractionModuleBase
         var botUser = Context.Client.CurrentUser;
         foreach (var message in messages)
         {
-            var messageClean = message.CleanContent.Replace(":","");
+            var messageClean = SanitizeString(message.CleanContent);
             if (message.Author.Id == botUser.Id)
             {
                 queryString += $"Wah: {messageClean}\n";
@@ -48,16 +49,16 @@ public class AIChatCmd : InteractionModuleBase
                 {
                     var replyID = message.Reference.MessageId;
                     var replyMessage = await Context.Channel.GetMessageAsync((ulong)replyID);
-                    if (replyMessage.Author.Id == botUser.Id)
-                        queryString += $"{message.Author.Username}: Wah, {messageClean}\n";
+                    if (replyMessage != null && replyMessage.Author.Id == botUser.Id)
+                        queryString += $"{SanitizeString(message.Author.Username)}: Wah, {messageClean}\n";
                 }
                 else
                 {
-                    queryString += $"{message.Author.Username}: {messageClean}\n";
+                    queryString += $"{SanitizeString(message.Author.Username)}: {messageClean}\n";
                 }
-
             }
         }
+
         queryString = queryString.Replace($"@{botUser.Username}#{botUser.Discriminator}", "Wah");
         queryString += "Wah: ";
         await ModifyOriginalResponseAsync(r => r.Content += "Messages loaded, Querying APi");
@@ -77,7 +78,7 @@ public class AIChatCmd : InteractionModuleBase
             Stop = "\n"
         }, OpenAI.GPT3.ObjectModels.Models.Davinci);
         if (!completionResult.Successful) throw new Exception("API Error: " + completionResult.Error);
-        var aiText = completionResult.Choices.FirstOrDefault().Text;
+        var aiText = SanitizeString(completionResult.Choices.FirstOrDefault().Text);
         var cost = (decimal)(completionResult.Usage.TotalTokens * 0.000002);
         await ModifyOriginalResponseAsync(r => r.Content = "Response received, moderating content");
         //check the response is not offensive using the OpenAI moderation API
@@ -107,13 +108,16 @@ public class AIChatCmd : InteractionModuleBase
         //Respond
         if (!moderationFlagged && aiText != "" && !blacklistFlagged)
             //Send message
+        {
             await ReplyAsync(aiText);
+        }
         else
         {
             var builder = new ComponentBuilder()
                 .WithButton("Override Filter", "aiChatApprove", ButtonStyle.Success);
             await ModifyOriginalResponseAsync(m => m.Components = builder.Build());
         }
+
         //Log
         var resultEmbed = await AuditLog.LogEvent(Context.Client as DiscordSocketClient,
             "***Message Processed***\n" +
@@ -126,5 +130,10 @@ public class AIChatCmd : InteractionModuleBase
         await ModifyOriginalResponseAsync(r => r.Content = "Content moderated, processing response");
         await ModifyOriginalResponseAsync(m => m.Embed = resultEmbed.Build());
         typingObj.Dispose();
+    }
+
+    private static string SanitizeString(string str)
+    {
+        return Regex.Replace(str, "[^a-zA-Z0-9 ,?'`.\"]", "", RegexOptions.Compiled);
     }
 }

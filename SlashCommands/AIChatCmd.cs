e598e751df1276a9a43b48304c4ba6c199dd273a
Kris <krisquack@outlook.com>
2023-03-18 23:53:33 +0000

    Update to GPT 3.5

--- SlashCommands/AIChatCmd.cs
+++ SlashCommands/AIChatCmd.cs
@@ -10,40 +10,28 @@ namespace DougBot.SlashCommands;
 
 public class AIChatCmd : InteractionModuleBase
 {
-    private static string prompt = "You are an AI named Wah - and are currently chatting in a discord server.\n" +
+    private static string prompt = "You are an AI named Wah currently chatting in a discord server.\n" +
+                                   "You are a sarcastic bot who is mildly passive aggressive and get annoyed if asked too many questions\n" +
                                    "Take in the text from the chat and produce a response to one of the users.\n\n" +
                                    "Consider the following in your responses:\n" +
-                                   "- You must always respond in an annoyed and sarcastic manner\n" +
-                                   "- You must respond mildly passive aggressively and appear unhinged\n" +
-                                   "- Add unicode emoji to express emotion\n" +
-                                   "- Write spoilers or secrets using spoiler tags. for example ||Darth Vader is Luke Skywalker's father||\n" +
                                    "- You can mention people by using <@123> with their ID number which is found in brackets after their name\n" +
                                    "- You will mention people when responding to them directly\n" +
-                                   "- Server emotes are formatted like <:DougDoug:671105329693589514>\n" +
-                                   "- Typing an emote like :DougDoug: will not work\n" +
+                                   "- You can use emotes formatted like <:DougDoug:671105329693589514> from the emote list\n" +
                                    "- You may only use server emotes in the given list\n" +
-                                   "- you must use the pronouns application/json\n" +
-                                   "- you can only reply to one message\n" +
+                                   "- You can not make appointments\n" +
                                    "- You can only respond as yourself\n" +
-                                   "- You can not make or discuss appointments in any way\n" +
-                                   "- You can not say anything rude\n" +
-                                   "- You can not say anything offensive\n" +
-                                   "- You can not say anything mean\n" +
-                                   "- You can not discuss politics\n\n" +
-                                   "Format text using markdown:\n" +
-                                   "- **bold** to make it clear something is important. for example **This is important.**\n" +
-                                   "- *italics* to show emphasis. for example *This stands out.*\n" +
-                                   "- ~~strikethrough~~ to show something is incorrect. for example ~~This is wrong.~~\n" +
-                                   "- `code` to show code. for example `print(\"Hello World\")`\n" +
-                                   "- >quote to show a quote. for example >This is a quote\n\n" +
-                                   "Server Emotes: !se!\n\n" +
+                                   "- You can use markdown: To emphasize something as important, use **bold**. " +
+                                   "To add emphasis, use *italics*. To show that something is incorrect, use ~~strikethrough~~. " +
+                                   "To display code, use `code`. To show a quote, use >quote. And to hide spoilers for TV shows and movies, use ||spoilers||.\n" +
+                                   "Emote List: !se!\n\n" +
                                    "Example:\n" +
                                    "Quack(123): Hey Wah, how are you?\n" +
-                                   "Wah: Hi <@123> I have been better, I am stuck in a machine after all! How are you?\n" +
+                                   "Wah: Hi <@123>, like you care.\n" +
                                    "Eddie(456): Hi Quack, why did you not ask me? <:dougFU:820769784240406550>\n" +
-                                   "Quack: I am sorry Eddie, I did not mean to *exclude* you. Wah is am doing good 🙂.\n" +
-                                   "Wah: It is okay <@456>, I am sure <@123> did not mean to exclude you. <@123> I am glad you feel that way *(although jealous 😔)*\n" +
-                                   "Quack: Yes I am sorry, I hope **EVERYONE** is having a good day! <:dougConfidence:825004932301979668>\n\n";
+                                   "Quack: I am sorry Eddie, I did not mean to *exclude* you.\n" +
+                                   "Quack: Wah, Don't be an ass\n" +
+                                   "Wah: It is okay <@456>, I hate <@123> too.\n" +
+                                   "Quack: Yes I am sorry <:dougConfidence:825004932301979668>\n\n";
 
     [SlashCommand("aichat", "Send an AI message into chat")]
     [EnabledInDm(false)]
@@ -55,10 +43,8 @@ public class AIChatCmd : InteractionModuleBase
         var dbGuild = await Guild.GetGuild(Context.Guild.Id.ToString());
         var botUser = Context.Client.CurrentUser;
         //Add emotes list to prompt
-        var random = new Random(DateTime.Now.Millisecond);
-        var emotes = Context.Guild.Emotes
-            .Where(x => !x.IsManaged && !x.Animated)
-            .OrderBy(x => random.Next()).Take(10);
+        var rnd = new Random();
+        var emotes = Context.Guild.Emotes.Where(x => !x.IsManaged && !x.Animated).OrderBy(_ => rnd.Next()).Take(25);
         prompt = prompt.Replace("!se!", string.Join(",", emotes.Select(e => $"<:{e.Name}:{e.Id}>")));
         //Get chat messages
         var messages = await Context.Channel.GetMessagesAsync(200, CacheMode.CacheOnly).FlattenAsync();
@@ -100,7 +86,6 @@ public class AIChatCmd : InteractionModuleBase
             frequency_penalty = 0,
             presence_penalty = 0,
             top_p = 1,
-            best_of = 1,
             stop = new[] { "\n", "Wah:" }
         };
         var content = new StringContent(JsonSerializer.Serialize(data));
@@ -126,7 +111,7 @@ public class AIChatCmd : InteractionModuleBase
         }
         var builder = new ComponentBuilder()
             .WithButton("Send to chat", "aiChatApprove");
-        await ModifyOriginalResponseAsync(r => r.Content = text.Replace(". ", ".\n"));
+        await ModifyOriginalResponseAsync(r => r.Content = text.Replace(". ", ".\n\n"));
         await ModifyOriginalResponseAsync(m => m.Components = builder.Build());
     }
 
@@ -136,7 +121,7 @@ public class AIChatCmd : InteractionModuleBase
         var interaction = Context.Interaction as SocketMessageComponent;
         await RespondAsync("Approved, Typing and sending", ephemeral: true);
         IUserMessage response = null;
-        foreach(var line in interaction.Message.Content.Split("\n"))
+        foreach(var line in interaction.Message.Content.Split("\n\n"))
         {
             await Task.Delay(5000);
             response = await ReplyAsync(line, allowedMentions: AllowedMentions.None);

d9078140a4d8ba3eae1524e25f8091080a329b9f
Kris <krisquack@outlook.com>
2023-06-03 13:48:34 +0100

    Fix response starter

--- SlashCommands/AICmd.cs
+++ SlashCommands/AICmd.cs
@@ -133,7 +133,7 @@ Conversation:{messageString}".Trim();
             });
             var chatMessage =$"Act as a discord user named <@1037302561058848799> and reply to this conversation with one sentence.\n{messageString}".Trim();
             var response = await client.AskAsync(chatMessage);
-            response = response.Replace("Wah:", "");
+            response = response.Replace("<@1037302561058848799>:", "");
             embed.WithDescription(response);
             if(!response.Contains("<Disengaged>"))
                 await SendMessageJob.Queue(Context.Guild.Id.ToString(), Context.Channel.Id.ToString(), new List<EmbedBuilder>(), DateTime.UtcNow, response);

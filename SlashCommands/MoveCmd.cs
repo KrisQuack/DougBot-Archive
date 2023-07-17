e0009efbc1eec0c7b3d04c55d1d9734083646668
Kris <krisquack@outlook.com>
2023-07-17 23:14:19 +0100

    Fix channel typing

--- SlashCommands/MoveCmd.cs
+++ SlashCommands/MoveCmd.cs
@@ -15,16 +15,24 @@ public class MoveCmd : InteractionModuleBase
         [Summary(description: "The ID of the message")]
         string message,
         [Summary(description: "The channel to move it to")]
-        IChannel channelInput
+        IChannel channel
     )
     {
         await RespondAsync("Moving the message", ephemeral: true);
         //Identify if it is a thread
-        SocketThreadChannel? threadChannel = channelInput as SocketThreadChannel;
-        var channel = threadChannel != null
-            ? threadChannel.ParentChannel as ITextChannel
-            : channelInput as ITextChannel;
-        ulong? threadId = threadChannel != null ? threadChannel.Id : null;
+        SocketThreadChannel? threadChannel = channel as SocketThreadChannel;
+        ITextChannel? textChannel = null;
+        IForumChannel? forumChannel = null;
+        if (threadChannel != null)
+        {
+            textChannel = threadChannel.ParentChannel as ITextChannel;
+            forumChannel = threadChannel.ParentChannel as IForumChannel;
+        }
+        else
+        {
+            textChannel = channel as ITextChannel;
+            forumChannel = channel as IForumChannel;
+        }
         //Grab the message using a reply
         var messageToMove = await Context.Channel.GetMessageAsync(Convert.ToUInt64(message));
         //Check the message to move is not null
@@ -37,10 +45,23 @@ public class MoveCmd : InteractionModuleBase
         //Set the authors name as either the server nickname if there is one or the username
         var authorObj = messageToMove.Author as IGuildUser;
         var authorName = authorObj.Nickname ?? authorObj.Username;
-        //Check if the channel has a webhook named Wah
-        var webhooks = await channel.GetWebhooksAsync();
-        //If the webhook is null, create a new one
-        var wahWebhook = webhooks.FirstOrDefault(x => x.Name == "Wahaha") ?? await channel.CreateWebhookAsync("Wahaha");
+        //Get the webhook
+        IWebhook wahWebhook;
+        if (textChannel != null)
+        {
+            var webhooks = await textChannel.GetWebhooksAsync();
+            wahWebhook = webhooks.FirstOrDefault(x => x.Name == "Wahaha") ?? await textChannel.CreateWebhookAsync("Wahaha");
+        }
+        else if (forumChannel != null)
+        {
+            var webhooks = await forumChannel.GetWebhooksAsync();
+            wahWebhook = webhooks.FirstOrDefault(x => x.Name == "Wahaha") ?? await forumChannel.CreateWebhookAsync("Wahaha");
+        }
+        else
+        {
+            await ModifyOriginalResponseAsync(x => x.Content = "Invalid channel type. Only text channels, forum channels and threads are supported.");
+            return;
+        }
         //Grab the webhook
         var webhook = new DiscordWebhookClient(wahWebhook.Id, wahWebhook.Token);
         //Get the embeds
@@ -68,7 +89,7 @@ public class MoveCmd : InteractionModuleBase
             //Send the message with attachments
             await webhook.SendFilesAsync(attachments, messageToMove.Content, embeds: embedList,
                 username: authorName, avatarUrl: messageToMove.Author.GetAvatarUrl(),
-                allowedMentions: AllowedMentions.None, threadId: threadId);
+                allowedMentions: AllowedMentions.None, threadId: threadChannel.Id);
             //Delete the attachments
             foreach (var attachment in attachmentPaths) File.Delete(attachment);
         }
@@ -76,12 +97,11 @@ public class MoveCmd : InteractionModuleBase
         {
             await webhook.SendMessageAsync(messageToMove.Content, embeds: embedList,
                 username: authorName, avatarUrl: messageToMove.Author.GetAvatarUrl(),
-                allowedMentions: AllowedMentions.None, threadId: threadId);
+                allowedMentions: AllowedMentions.None, threadId: threadChannel.Id);
         }
 
         await messageToMove.DeleteAsync();
         await ModifyOriginalResponseAsync(x => x.Content = "Message moved");
-        string destination = threadChannel != null ? threadChannel.Mention : channel.Mention;
-        await ReplyAsync($"{messageToMove.Author.Mention} your message has been moved to {destination}");
+        await ReplyAsync($"{messageToMove.Author.Mention} your message has been moved to <#{channel.Id}>");
     }
 }
